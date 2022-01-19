d92e3783dd4f9ab8c73e2fa10d13989922ceba27
Robinson Junior <robinson.juniorr@gmail.com>
2022-01-18 22:21:02 -0300

    adicionando icones nas abas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
 import { Home } from "../screens/Home";
 import { Search } from "../screens/Search";
 import { Favorites } from "../screens/Favorites";
@@ -11,10 +13,28 @@ const Tab = createBottomTabNavigator();
 export function Routes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap =
+              "alert-circle-outline";
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Search") {
+              iconName = focused ? "search" : "search-outline";
+            } else if (route.name === "Favorites") {
+              iconName = focused ? "md-star" : "md-star-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#000000",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Buscar" component={Search} />
-        <Tab.Screen name="Favoritos" component={Favorites} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Favorites" component={Favorites} />
       </Tab.Navigator>
     </NavigationContainer>
   );
